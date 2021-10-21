7e2304b9a3da54735912161a8a0a4a0fb0f13763
Komal Gill <komalgill0310@gmail.com>
2021-10-21 09:49:19 -0400

    calculator is ready

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ const numberButtons = document.querySelectorAll('.buttonNumber');
 numberButtons.forEach(numberButton => {
   numberButton.addEventListener('click', (e) => {    
     if(!operator){
-      firstOperand = e.target.value;
-      display.value += firstOperand;
-      console.log(display.value);
+      firstOperand += e.target.value;
+      display.value = firstOperand;
     }
     else{
-      secondOperand = e.target.value;     
-      display.value += secondOperand;
-      console.log(display.value);
+      secondOperand += e.target.value;     
+      display.value = secondOperand;
     }
   });
 });
@@ -23,116 +21,42 @@ numberButtons.forEach(numberButton => {
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(operatorButton => {
   operatorButton.addEventListener('click', (e) => {
-     if(!operator){
-       operator = e.target.value;
-       display.value += operator;
-       console.log(display.value);
-     }
-     else{
-       switch(operator){
-       case"+":
-       display.value = parseInt(firstOperand) + parseInt(secondOperand);
-       break;
-       case"-":
-       display.value = parseInt(firstOperand) - parseInt(secondOperand);
-       break;
-       case"X":
-       display.value = parseInt(firstOperand) * parseInt(secondOperand);
-       break;
-       default:
-       display.value = parseInt(firstOperand) / parseInt(secondOperand);
-       display.value = divisionNumber.toFixed(3);
-       break;
-      }
-      let newOperator = e.target.value;
-      display.value += newOperator;
-      console.log(display.value); 
-     }
+      if(firstOperand && secondOperand){
+        calculate();
+        }
+      else{
+        operator = e.target.value;
+        display.value = operator;
+      }          
   });
-
-const equalButton = document.querySelector('#equalSign');
-equalButton.addEventListener('click', () => {
-    if(operator==="+"){
-      display.value = parseInt(firstOperand) + parseInt(secondOperand);
-    } 
-    else if(operator==="-"){
-      display.value = parseInt(firstOperand) - parseInt(secondOperand);
-    }
-    else if(operator==="X"){
-      display.value = parseInt(firstOperand) * parseInt(secondOperand);
-    }
-    else{
-      let divisionNumber = parseInt(firstOperand)/
-      parseInt(secondOperand);
-      display.value = divisionNumber.toFixed(3);
-    }
 });
 
+const equalButton = document.querySelector('#equalSign');
+equalButton.addEventListener('click', () => calculate());
+    
 const resetButton = document.querySelector('#clearButton');
 resetButton.addEventListener('click', () => {
   display.value = "0";
 });
 
+function calculate(){
+  let result;
+   switch(operator){
+    case"+":
+     result=parseInt(firstOperand)+parseInt(secondOperand);
+    break;
+   case"-":
+     result=parseInt(firstOperand)-parseInt(secondOperand);
+    break;
+   case"X":
+     result=parseInt(firstOperand)*parseInt(secondOperand);
+    break;
+   default:
+     result=parseInt(firstOperand)/parseInt(secondOperand);
+    break;
+   }        
+    firstOperand = result;
+    secondOperand = "";
+    display.value = result;
+}
 
-// function calculate(firstOperand, operator, secondOperand){
-//   let result; 
-
-//   switch(operator){
-//     case"+":
-//     result = parseInt(firstOperand) + parseInt(secondOperand);
-//     break;
-//   }
-//   console.log(result);
-//   return result;
-// }
-
-// return (operator==="+")? display.value = parseInt(firstOperand) + parseInt(secondOperand):
-// (operator==="-")?display.value = parseInt(firstOperand) - parseInt(secondOperand):
-// operator==="X"? console.log(parseInt(firstOperand)*
-// parseInt(secondOperand)) :
-// display.value = parseInt(firstOperand) / parseInt(secondOperand);
-
-    // if(!operator){
-    //   operator = e.target.value;
-    //   display.value += operator;
-    //   console.log(display.value);
-    // }
-  
-    // if(firstOperand,operator,secondOperand!=0) {
-    //   if(!!operator){
-    //     if(operator==="-"){
-    //       display.value = parseInt(firstOperand) -parseInt(secondOperand);
-    //       display.value += operator;
-    //       console.log(display.value);
-    //     } 
-    //   }
-    // }   
-    // else{
-    //   operator = e.target.value;
-    //   display.value += operator;
-    //   console.log(display.value); 
-    // }   
-    
-    // if(firstOperand && secondOperand){
-    //   //calculate
-    //   if(operator==="+"){
-    //     display.value = parseInt(firstOperand) + parseInt(secondOperand);
-    //   } 
-    //   else if(operator==="-"){
-    //     display.value = parseInt(firstOperand) - parseInt(secondOperand);
-    //   }
-    //   else if(operator==="X"){
-    //     display.value = parseInt(firstOperand) * parseInt(secondOperand);
-    //   }
-    //   else{
-    //     let divisionNumber = parseInt(firstOperand)/
-    //     parseInt(secondOperand);
-    //     display.value = divisionNumber.toFixed(3);
-    //   }
-    //   }
-    //   else{
-    //     operator = e.target.value;
-    //     display.value += operator;
-    //     console.log(display.value);
-    //   }  
-  });
